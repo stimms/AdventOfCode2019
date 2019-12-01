2c6218c069f6c5354d52fec6a0db11e743a723ca
Simon Timms <stimms@gmail.com>
2019-11-30 22:18:35 -0700

    Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var lines = File.ReadAllLines("input.txt");
+            double total = 0;
+            double runningTotal;
+            foreach (var line in lines)
+            {
+                runningTotal = Double.Parse(line);
+                do
+                {
+                    var temp = Math.Floor(runningTotal / 3) - 2;
+                    if (temp > 0)
+                        total += temp;
+                    runningTotal = temp;
+                } while (runningTotal > 0);
+            }
+            Console.WriteLine(total);
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Template/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var lines = File.ReadAllLines("input.txt");
+            foreach(var line in lines){
+                var intval = Int.Parse(line);
+            }
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+    }
+}
