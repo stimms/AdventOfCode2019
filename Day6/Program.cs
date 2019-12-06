8572c49bb748feb384d159cb385d54170cde3520
Simon Timms <stimms@gmail.com>
2019-12-05 22:58:03 -0700

    Day 6 refactor

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -9,7 +9,7 @@ namespace Advent
     class Node
     {
         public string Name { get; set; }
-        public Node Parent {get;set;}
+        public Node Parent { get; set; }
         public List<Node> Children { get; set; } = new List<Node>();
     }
     class Program
@@ -22,9 +22,9 @@ namespace Advent
             {
                 var newNode = new Node { Name = toAdd, Parent = root };
                 root.Children.Add(newNode);
-                if(newNode.Name == "YOU")
+                if (newNode.Name == "YOU")
                     YOU = newNode;
-                if(newNode.Name == "SAN")
+                if (newNode.Name == "SAN")
                     SAN = newNode;
                 return true;
             }
@@ -32,23 +32,28 @@ namespace Advent
             {
                 foreach (var child in root.Children)
                 {
-                    if(Add(child, parent, toAdd))
+                    if (Add(child, parent, toAdd))
                         return true;
                 }
             }
             return false;
         }
 
-        public static int Visit(Node root, int levelCount)
+        public static int CountOrbits(Node root, int levelCount)
         {
-
-            return root.Children.Select(x => Visit(x, levelCount + 1) + levelCount).Sum(); ;
+            return root.Children.Select(x => CountOrbits(x, levelCount + 1) + levelCount).Sum(); ;
         }
 
-        public static string GetParentChain(Node leaf){
-            if(leaf.Parent != null)
-                return GetParentChain(leaf.Parent) + "," + leaf.Name;
-            return "";
+        public static List<string> GetParentChain(Node leaf)
+        {
+            if (leaf.Parent != null)
+            {
+                var temp = GetParentChain(leaf.Parent);
+                temp.Add(leaf.Name);
+                return temp;
+            }
+
+            return new List<string>();
         }
 
         static void Main(string[] args)
@@ -56,42 +61,57 @@ namespace Advent
             Console.WriteLine("Starting");
 
             var lines = File.ReadAllLines("input.txt").ToList();
-            
+
             var root = new Node { Name = "COM" };
+            BuildOrbits(lines, root);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(CountOrbits(root, 1));
+            Console.ResetColor();
+            FindCommonAncestor();
+
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+
+        private static void FindCommonAncestor()
+        {
+            var youParents = GetParentChain(YOU);
+            var sanParents = GetParentChain(SAN);
+            Console.WriteLine(String.Join(",", youParents));
+            Console.WriteLine(String.Join(",", sanParents));
+
+
+            youParents.Reverse();
+            sanParents.Reverse();
+            int youLevel = 0;
+            foreach (var youParent in youParents)
+            {
+                var index = sanParents.FindIndex(x => x == youParent);
+                if (index >= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(youParent + ":" + (index + youLevel - 2));
+                    Console.ResetColor();
+                    break;
+                }
+                youLevel++;
+            }
+        }
+
+        private static void BuildOrbits(List<string> lines, Node root)
+        {
             while (lines.Any())
             {
                 var visited = new List<string>();
                 foreach (var line in lines)
                 {
-                    if(Add(root, line.Split(")")[0], line.Split(")")[1]))
+                    if (Add(root, line.Split(")")[0], line.Split(")")[1]))
                         visited.Add(line);
                 }
-                lines = lines.Where(x=> !visited.Contains(x)).ToList();
-                //Console.WriteLine(lines.Count());
+                lines = lines.Where(x => !visited.Contains(x)).ToList();
+                //Console.WriteLine("To process: " + lines.Count());
             }
-            var counter = 0;
-            counter = Visit(root, 1);
-            Console.WriteLine(counter);
-
-            var youParents = GetParentChain(YOU);
-            var sanParents = GetParentChain(SAN);
-            Console.WriteLine(youParents);
-            Console.WriteLine(sanParents);
-
-            int youLevel = 0;
-            foreach(var youParent in youParents.Split(",").Reverse())
-            {
-                var index = sanParents.Split(",").Reverse().ToList().FindIndex(x=>x==youParent);
-                if(index>=0)
-                    {
-                        Console.WriteLine(youParent + ":"+ index + youLevel);
-                        break;
-                    }
-                youLevel++;
-            }
-
-            Console.WriteLine("done.");
-            Console.ReadLine();
         }
     }
 }
