8ea5cbc91ba49038d9210210b83bf2538cc55aac
Simon Timms <stimms@gmail.com>
2019-12-12 21:53:17 -0700

    Before day 13

---
+++ Day13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var lines = File.ReadAllLines("input.txt");
+            foreach(var line in lines){
+                var intval = Int32.Parse(line);
+            }
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+    }
+}
