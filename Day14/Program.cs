7038ea6120e970b77b31ff9adcf1f43d5d7da06f
Simon Timms <stimms@gmail.com>
2019-12-14 21:57:45 -0700

    Day14

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -7,19 +7,19 @@ namespace Advent
 {
     class Reaction
     {
-        public IList<(int qty, string chemical)> inputs { get; set; }
-        public (int qty, string chemical) output { get; set; }
+        public IList<(long qty, string chemical)> inputs { get; set; }
+        public (long qty, string chemical) output { get; set; }
     }
 
     class Program
     {
         static List<Reaction> reactions;
-        static Dictionary<string, int> surpluss = new Dictionary<string, int>();
-        static int generate(string chemical, int qty)
+        static Dictionary<string, long> surpluss = new Dictionary<string, long>();
+        static long generate(string chemical, long qty)
         {
-            var result = 0;
+            var result = 0L;
             var reaction = reactions.Where(x => x.output.chemical == chemical).Single();
-            var multiplier = (int)Math.Ceiling((double)qty / (double)reaction.output.qty);
+            var multiplier = (long)Math.Ceiling((double)qty / (double)reaction.output.qty);
             foreach (var input in reaction.inputs.Select(x => (qty: x.qty, x.chemical)))
             {
                 if (!surpluss.ContainsKey(chemical))
@@ -48,30 +48,43 @@ namespace Advent
             Console.WriteLine("Starting");
             foreach (var file in new string[] {
                 //"input.test1.txt",
-                "input.test2.txt",
-                "input.test3.txt",
-                "input.test4.txt",
-                "input.test5.txt",
+                // "input.test2.txt",
+                // "input.test3.txt",
+                // "input.test4.txt",
+                // "input.test5.txt",
                 "input.txt" })
             {
                 var lines = File.ReadAllLines(file);
-                surpluss = new Dictionary<string, int>();
+                surpluss = new Dictionary<string, long>();
                 reactions = new List<Reaction>();
                 foreach (var line in lines)
                 {
                     var tempInputs = line.Split("=>").First().Split(",");
-                    var inputs = new List<(int, string)>();
+                    var inputs = new List<(long, string)>();
                     foreach (var temp in tempInputs)
                     {
-                        var qty = Int32.Parse(temp.Trim().Split(" ").First());
+                        var qty = long.Parse(temp.Trim().Split(" ").First());
                         var chemical = temp.Trim().Split(" ").Last();
                         inputs.Add((qty, chemical));
                     }
                     var tempChemical = line.Split("=>").Last().Trim().Split(" ");
-                    reactions.Add(new Reaction { inputs = inputs, output = (Int32.Parse(tempChemical.First()), tempChemical.Last()) });
+                    reactions.Add(new Reaction { inputs = inputs, output = (long.Parse(tempChemical.First()), tempChemical.Last()) });
                 }
                 Console.WriteLine(generate("FUEL", 1));
-
+                var upper = 1_000_000_000L;
+                var lower = 0L;
+                while(upper>lower)
+                {
+                    var midpoint = lower + (upper-lower)/2;
+                    var generated = generate("FUEL", midpoint);
+                    Console.WriteLine(midpoint + " " + generated);
+                    if(generated>1_000_000_000_000L)
+                        upper = midpoint;
+                    else
+                        lower = midpoint;
+                }
+                Console.WriteLine(upper);
+                Console.WriteLine(lower);
 
 
             }
