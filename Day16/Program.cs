6c5c5082018073dfbe302a09b6d097454263deda
Simon Timms <stimms@gmail.com>
2019-12-16 06:17:44 -0700

    Part 2

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -16,39 +16,37 @@ namespace Advent
             var phases = 100;
             var inputSet = input.ToCharArray().Select(x => Int32.Parse(x.ToString())).ToList();
             var workingSet = new List<int>();
-            for (int i = 0; i < 10_000; i++)
-                workingSet.AddRange(inputSet);
+            workingSet.AddRange(inputSet);
             var output = new List<int>();
             for (int i = 0; i < phases; i++)
             {
                 Console.WriteLine(i);
-                int digitCounter = 1;
+                int digitCounter = 5978261;
                 output = new List<int>();
-                foreach (var c in workingSet)
+                foreach (var c in workingSet.Skip(5978261))
                 {
+
                     var multipliers = new List<int>();
-                    while (multipliers.Count < workingSet.Count + 2)
-                    {
-                        for (int j = 0; j < digitCounter; j++)
-                            multipliers.Add(0);
-                        for (int j = 0; j < digitCounter; j++)
-                            multipliers.Add(1);
-                        for (int j = 0; j < digitCounter; j++)
-                            multipliers.Add(0);
-                        for (int j = 0; j < digitCounter; j++)
-                            multipliers.Add(-1);
-                    }
+                    for (int j = 0; j < digitCounter; j++)
+                        multipliers.Add(0);
+                    for (int j = 0; j < digitCounter; j++)
+                        multipliers.Add(1);
+                    for (int j = 0; j < digitCounter; j++)
+                        multipliers.Add(0);
+                    for (int j = 0; j < digitCounter; j++)
+                        multipliers.Add(-1);
+
                     var total = 0;
                     var multiplier = digitCounter;
                     for (int nonZero = digitCounter - 1; nonZero < workingSet.Count; nonZero += 0)
                     {
 
-                        total += multipliers[multiplier] * workingSet[nonZero];
+                        total += multipliers[multiplier % (digitCounter * 4)] * workingSet[nonZero];
                         multiplier++;
-                        if (multipliers[multiplier] == 0)
+                        if (multipliers[multiplier % (digitCounter * 4)] == 0)
                         {
                             nonZero += digitCounter + 1;
-                            multiplier+=digitCounter;
+                            multiplier += digitCounter;
                         }
                         else
                             nonZero++;
@@ -58,10 +56,33 @@ namespace Advent
                 }
                 workingSet = output;
             }
-            var offset = Int32.Parse(String.Join("", output.Take(7)));
+            Console.WriteLine(String.Join("", output));
+
+            var offset = Int32.Parse(String.Join("", input.Take(7)));
             Console.WriteLine(offset);
-            Console.WriteLine(String.Join("", output.Skip(offset).Take(8)));
-            //Console.WriteLine(String.Join("", output));
+
+            workingSet.Clear();
+            for (int i = 0; i < 10_000; i++)
+                workingSet.AddRange(inputSet);
+            var holder = new List<int>();
+            holder.AddRange(workingSet.Skip(offset));
+            for (int i = 0; i < 100; i++)
+            {
+                var temp = new List<int>();
+                var partialSum = holder.Sum();
+                
+                temp.Add(Math.Abs(partialSum) % 10);
+                for (int j = 0; j < holder.Count(); j++)
+                {
+                    partialSum -= holder[j];
+                    temp.Add(Math.Abs(partialSum) % 10);
+                }
+                holder = temp;
+            }
+
+
+            Console.WriteLine(String.Join("", holder.Take(8)));
+
 
             Console.WriteLine("done.");
             Console.ReadLine();
